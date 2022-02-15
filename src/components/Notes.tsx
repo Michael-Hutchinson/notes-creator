d04723778b9ce1d6554ae4910e31c9908c446868
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-15 19:02:26 +0000

    deconstruct handledelete prop and pass in id

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -7,7 +7,10 @@ interface INotesProps {
   handleDelete: (id: string) => void;
 }
 
-const Notes: React.FunctionComponent<INotesProps> = ({ note }) => {
+const Notes: React.FunctionComponent<INotesProps> = ({
+  note,
+  handleDelete,
+}) => {
   return (
     <>
       <div className="mb-3">
@@ -16,7 +19,11 @@ const Notes: React.FunctionComponent<INotesProps> = ({ note }) => {
             <Card.Title>{note.title}</Card.Title>
             <Card.Text>{note.text}</Card.Text>
             <Card.Subtitle className="text-muted">{note.date}</Card.Subtitle>
-            <Button className="mt-3" variant="danger" onClick={() => {}}>
+            <Button
+              className="mt-3"
+              variant="danger"
+              onClick={() => handleDelete(note.id)}
+            >
               Delete
             </Button>
           </Card.Body>
