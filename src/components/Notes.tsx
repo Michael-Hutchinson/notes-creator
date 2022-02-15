cd38c1c3ea3bb7a2a042a9a328d0de3a682e20c1
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-15 18:33:13 +0000

    crate notes component and return a div

---
+++ src/components/Notes.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Note } from '../models/note.model';
+
+interface INotesProps {
+  note: Note;
+}
+
+const Notes: React.FunctionComponent<INotesProps> = ({ note }) => {
+  return (
+    <>
+      <div>Note Card</div>
+    </>
+  );
+};
+
+export default Notes;
