5e66599b0a5c40621768db6abda6ab21d08883f1
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-16 11:46:08 +0000

    create setNotes to submit form

--- src/components/CreateNotes.tsx
+++ src/components/CreateNotes.tsx
@@ -1,9 +1,16 @@
 import React, { useRef, useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import { Note } from '../models/note.model';
 
-interface ICreateNotesProps {}
+interface ICreateNotesProps {
+  notes: Note[];
+  setNotes: React.Dispatch<React.SetStateAction<Note[]>>;
+}
 
-const CreateNotes: React.FunctionComponent<ICreateNotesProps> = (props) => {
+const CreateNotes: React.FunctionComponent<ICreateNotesProps> = ({
+  notes,
+  setNotes,
+}) => {
   const [error, setError] = useState<string>('');
   const titleRef = useRef<HTMLInputElement | null>(null);
   const textRef = useRef<HTMLTextAreaElement | null>(null);
@@ -12,44 +19,59 @@ const CreateNotes: React.FunctionComponent<ICreateNotesProps> = (props) => {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
     if (titleRef.current?.value === '' || textRef.current?.value === '') {
-      return setError('Please fill in all fields');
+      setError('Please fill in all fields');
     }
+    setError('');
+    setNotes([
+      ...notes,
+      {
+        id: new Date().toString(),
+        title: (titleRef.current as HTMLInputElement).value,
+        text: (textRef.current as HTMLTextAreaElement).value,
+        color: (colorRef.current as HTMLInputElement).value,
+        date: new Date().toString(),
+      },
+    ]);
+    (titleRef.current as HTMLInputElement).value = '';
+    (textRef.current as HTMLTextAreaElement).value = '';
   };
-  <>
-    <h2>Create Notes</h2>
-    <Form className="mt-3 mb-3" onSubmit={(e) => handleSubmit(e)}>
-      <Form.Group className="mb-3" controlId="formBasicTitle">
-        <Form.Label>Title:</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter Title for the Note"
-          ref={titleRef}
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicText">
-        <Form.Label>Text:</Form.Label>
-        <Form.Control
-          placeholder="Enter your notes"
-          as="textarea"
-          rows={3}
-          ref={textRef}
-        />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label htmlFor="colorInput">Notes Color</Form.Label>
-        <Form.Control
-          type="color"
-          id="colorInput"
-          defaultValue="#DFDFDF"
-          ref={colorRef}
-          title="Choose your color"
-        />
-      </Form.Group>
-      <Button type="submit" variant="primary">
-        Submit
-      </Button>
-    </Form>
-  </>;
+  return (
+    <>
+      <h2>Create Notes</h2>
+      <Form className="mt-3 mb-3" onSubmit={(e) => handleSubmit(e)}>
+        <Form.Group className="mb-3" controlId="formBasicTitle">
+          <Form.Label>Title:</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Title for the Note"
+            ref={titleRef}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicText">
+          <Form.Label>Text:</Form.Label>
+          <Form.Control
+            placeholder="Enter your notes"
+            as="textarea"
+            rows={3}
+            ref={textRef}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="colorInput">Notes Color</Form.Label>
+          <Form.Control
+            type="color"
+            id="colorInput"
+            defaultValue="#DFDFDF"
+            ref={colorRef}
+            title="Choose your color"
+          />
+        </Form.Group>
+        <Button type="submit" variant="primary">
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
 };
 
 export default CreateNotes;
